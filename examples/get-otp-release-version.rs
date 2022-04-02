5cf88b701c1fd33574706675ff8ee1a8585f0041
Takeru Ohta <phjgt308@gmail.com>
2022-04-02 23:03:19 +0900

    Add some components

--- examples/get-otp-release-version.rs
+++ examples/get-otp-release-version.rs
@@ -1,4 +1,5 @@
 use clap::Parser;
+use erl_dist::term::{Atom, List};
 
 #[derive(Debug, Parser)]
 struct Args {
@@ -20,6 +21,18 @@ fn main() -> anyhow::Result<()> {
 
     smol::block_on(async {
         let client = erl_rpc::RpcClient::connect(&args.node_name.to_string(), &cookie).await?;
+        let mut handle = client.handle();
+        smol::spawn(client.run()).detach();
+
+        let result = handle
+            .call(
+                "erlang".into(),
+                "system_info".into(),
+                List::from(vec![Atom::from("otp_release").into()]),
+            )
+            .await?;
+        eprintln!("Result: {:?}", result);
+
         Ok(())
     })
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,18 @@
 use erl_dist::epmd::{EpmdClient, NodeEntry};
 use erl_dist::handshake::{ClientSideHandshake, HandshakeStatus};
-use erl_dist::message::{self};
+use erl_dist::message::{self, Message};
 use erl_dist::node::{Creation, LocalNode, NodeName, PeerNode};
+use erl_dist::term::{Atom, List, Term};
 use erl_dist::DistributionFlags;
+use futures::channel::{mpsc, oneshot};
+use futures::FutureExt;
 use smol::net::TcpStream;
 
 #[derive(Debug, thiserror::Error)]
 #[non_exhaustive]
-pub enum Error {
+pub enum ConnectError {
     #[error(
-        "the server only supports the distribution protocol version 5 while this crate requires 6"
+        "the server only supports the distribution protocol version 5 while the client requires 6"
     )]
     TooOldDistributionProtocolVersion,
 
@@ -32,20 +35,32 @@ pub enum Error {
     IoError(#[from] std::io::Error),
 }
 
+#[derive(Debug, thiserror::Error)]
+#[non_exhaustive]
+pub enum CallError {
+    #[error("send buffer is full")]
+    Full,
+
+    #[error("RpcClient has been terminated")]
+    Terminated,
+}
+
 #[derive(Debug)]
 pub struct RpcClient {
-    tx: message::Sender<TcpStream>,
-    rx: message::Receiver<TcpStream>,
+    msg_rx: Option<message::Receiver<TcpStream>>,
+    msg_tx: message::Sender<TcpStream>,
+    req_rx: mpsc::Receiver<Request>,
+    req_tx: Option<mpsc::Sender<Request>>,
     local_node: LocalNode,
     peer_node: PeerNode,
 }
 
 impl RpcClient {
-    pub async fn connect(server_node_name: &str, cookie: &str) -> Result<Self, Error> {
+    pub async fn connect(server_node_name: &str, cookie: &str) -> Result<Self, ConnectError> {
         let server_node_name: NodeName = server_node_name.parse()?;
         let server_node_entry = get_node_entry(&server_node_name).await?;
         if server_node_entry.highest_version < 6 {
-            return Err(Error::TooOldDistributionProtocolVersion);
+            return Err(ConnectError::TooOldDistributionProtocolVersion);
         }
 
         let tentative_name = "nonode@localhost";
@@ -60,27 +75,152 @@ impl RpcClient {
             local_node.name = NodeName::new(&name, local_node.name.host())?;
             local_node.creation = creation;
             let (connection, peer_node) = handshake.execute_rest(true).await?;
-            let (tx, rx) = message::channel(connection, local_node.flags & peer_node.flags);
+            let (msg_tx, msg_rx) = message::channel(connection, local_node.flags & peer_node.flags);
+            let (req_tx, req_rx) = mpsc::channel(1024); // TODO: Remove hard-coding
             Ok(Self {
-                tx,
-                rx,
+                msg_tx,
+                msg_rx: Some(msg_rx),
+                req_rx,
+                req_tx: Some(req_tx),
                 local_node,
                 peer_node,
             })
         } else {
-            return Err(Error::UnexpectedHandshakeStatus { status });
+            return Err(ConnectError::UnexpectedHandshakeStatus { status });
+        }
+    }
+
+    pub fn handle(&self) -> RpcClientHandle {
+        RpcClientHandle {
+            req_tx: self.req_tx.clone().take().expect("unreachable"),
+        }
+    }
+
+    pub fn local_node(&self) -> &LocalNode {
+        &self.local_node
+    }
+
+    pub fn peer_ndoe(&self) -> &PeerNode {
+        &self.peer_node
+    }
+
+    pub async fn run(mut self) -> Result<(), RunError> {
+        self.req_tx = None;
+
+        let msg_rx = self.msg_rx.take().expect("unreachable");
+        let mut msg_rx_fut = msg_rx.recv_owned().boxed();
+
+        let tick_interval = std::time::Duration::from_secs(30);
+        let mut tick_timer = smol::Timer::after(tick_interval);
+
+        loop {
+            if smol::future::poll_once(&mut tick_timer).await.is_some() {
+                self.msg_tx.send(Message::Tick).await?;
+                tick_timer.set_after(tick_interval);
+            }
+
+            match smol::future::poll_once(&mut msg_rx_fut).await {
+                Some(Ok((msg, msg_rx))) => {
+                    msg_rx_fut = msg_rx.recv_owned().boxed();
+                    self.handle_msg(msg).await?;
+                    continue;
+                }
+                Some(Err(e)) => {
+                    return Err(e.into());
+                }
+                None => {}
+            }
+
+            match self.req_rx.try_next() {
+                Ok(Some(req)) => {
+                    self.handle_req(req).await?;
+                    continue;
+                }
+                Ok(None) => {
+                    break;
+                }
+                Err(_) => {}
+            }
+
+            let sleep_duration = std::time::Duration::from_millis(1);
+            smol::Timer::after(sleep_duration).await;
+        }
+        Ok(())
+    }
+
+    async fn handle_req(&mut self, req: Request) -> Result<(), RunError> {
+        todo!()
+    }
+
+    async fn handle_msg(&mut self, msg: Message) -> Result<(), RunError> {
+        todo!()
+    }
+}
+
+#[derive(Debug, thiserror::Error)]
+pub enum RunError {
+    #[error(transparent)]
+    MessageSendError(#[from] erl_dist::message::SendError),
+
+    #[error(transparent)]
+    MessageRecvError(#[from] erl_dist::message::RecvError),
+}
+
+#[derive(Debug, Clone)]
+pub struct RpcClientHandle {
+    req_tx: mpsc::Sender<Request>,
+}
+
+impl RpcClientHandle {
+    pub async fn call(
+        &mut self,
+        module: Atom,
+        function: Atom,
+        args: List,
+    ) -> Result<Term, CallError> {
+        let mfargs = MFArgs {
+            module,
+            function,
+            args,
+        };
+
+        let (reply_tx, reply_rx) = oneshot::channel();
+        let req = Request { mfargs, reply_tx };
+        if let Err(e) = self.req_tx.try_send(req) {
+            if e.is_disconnected() {
+                return Err(CallError::Terminated);
+            } else {
+                debug_assert!(e.is_full());
+                return Err(CallError::Full);
+            }
+        } else {
+            let term = reply_rx.await.map_err(|_| CallError::Terminated)?;
+            Ok(term)
         }
     }
 }
 
-async fn get_node_entry(node_name: &NodeName) -> Result<NodeEntry, Error> {
+#[derive(Debug)]
+struct MFArgs {
+    module: Atom,
+    function: Atom,
+    args: List,
+}
+
+#[derive(Debug)]
+struct Request {
+    mfargs: MFArgs,
+    reply_tx: oneshot::Sender<Term>,
+}
+
+async fn get_node_entry(node_name: &NodeName) -> Result<NodeEntry, ConnectError> {
     let connection =
         TcpStream::connect((node_name.host(), erl_dist::epmd::DEFAULT_EPMD_PORT)).await?;
     let client = EpmdClient::new(connection);
     if let Some(node) = client.get_node(node_name.name()).await? {
         Ok(node)
     } else {
-        Err(Error::NodeNodeFound {
+        Err(ConnectError::NodeNodeFound {
             name: node_name.clone(),
         })
     }
